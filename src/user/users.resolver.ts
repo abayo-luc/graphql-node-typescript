9e049e3e44382660425fcea3697724fc981f5d2d
AJL <luc.bayo@gmail.com>
2023-04-01 14:48:32 +0200

    chore: add time out to demostrate the role of nested resolver

--- src/user/users.resolver.ts
+++ src/user/users.resolver.ts
@@ -1,10 +1,15 @@
+const timeout = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 export const userResolvers = {
-  role: (
+  role: async (
     parent: any,
     _args: any,
     _context: any,
     _info: any
   ) => {
+    await timeout(5000);
     return parent.role.toUpperCase();
   },
 };
